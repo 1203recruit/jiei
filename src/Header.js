a0ffe0dbab2300cdd063d4e14429085f2ab5dbde
1203recruit <89901005+1203recruit@users.noreply.github.com>
2022-08-18 22:10:49 +0900

    CSS update

--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
       <header className="header">
         <div className="header-wrapper">
           <div className="logo-wrap">
-            <NavLink activeClassNAme="active" to="/">
-              <div className="logo neon">
+            <NavLink activeClassName="active" to="/" className="logo-wrapper non-select">
+              <div className="logo">
                 <j>J</j>iei
               </div>
             </NavLink>
@@ -17,46 +17,46 @@ const Header = () => {
           <nav className="nav">
             <ul className="list-wrapper">
               <NavLink
-                activeClassNAme="active"
+                activeClassName="active"
                 to={"/About/"}
-                className="underline"
+                className="underline non-select"
               >
                 <li className="li-item button-text">About</li>
               </NavLink>
               <NavLink
-                activeClassNAme="active"
+                activeClassName="active"
                 to={"/Works/"}
-                className="underline"
+                className="underline non-select"
               >
                 <li className="li-item button-text dropdown-menu">Works
                 <div className="dropdown-list">
                   <ul>
                     <li className="dropdown-item">
-                      <NavLink to={"/Works/"} className="button-text">Web site</NavLink>
+                      <NavLink to={"/Works/"} className="button-text non-select">Web site</NavLink>
                     </li>
                     <li className="dropdown-item">
-                      <NavLink to={"/Works/"} className="button-text">Logo</NavLink>
+                      <NavLink to={"/Works/"} className="button-text non-select">Logo</NavLink>
                     </li>
                     <li className="dropdown-item">
-                      <NavLink to={"/Works/"} className="button-text">Illust</NavLink>
+                      <NavLink to={"/Works/"} className="button-text non-select">Illust</NavLink>
                     </li>
                     <li className="dropdown-item">
-                      <NavLink to={"/Works/"} className="button-text">Other</NavLink>
+                      <NavLink to={"/Works/"} className="button-text non-select">Other</NavLink>
                     </li>
                   </ul>
                   </div>
                 </li>
               </NavLink>
               <NavLink
-                activeClassNAme="active"
+                activeClassName="active"
                 to={"/Contact/"}
-                className="underline"
+                className="underline non-select"
               >
                 <li className="li-item button-text">Contact</li>
               </NavLink>
             </ul>
           </nav>
-          <div className="github-icon">
+          <div className="github-icon non-select">
             <a href="https://github.com/1203recruit" target={"blank"}>
               <img src={GithubIcon} alt="Github-icon" />
             </a>
